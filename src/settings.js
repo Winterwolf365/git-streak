9d37778cb553be20c5e8a94f70fdd6df8389cee5
Pieter Verweij <pieterverweij2006@outlook.com>
2024-04-14 17:47:55 +0200

    add toggle to count commits from all authors

--- src/settings.js
+++ src/settings.js
@@ -5,6 +5,7 @@ async function get_settings() {
 
     document.querySelector("#startup").checked = settings[0];
     document.querySelector("#notifications").checked = settings[1];
+    document.querySelector("#all_authors").checked = settings[2];
 }
 
 async function set_startup() {
@@ -13,4 +14,9 @@ async function set_startup() {
 
 async function set_notifications() {
     await invoke('set_notifications_setting', { notifications: document.querySelector("#notifications").checked });
+}
+
+async function set_all_authors() {
+    await invoke('set_all_authors_setting', { allAuthors: document.querySelector("#all_authors").checked });
+    reload();
 }
